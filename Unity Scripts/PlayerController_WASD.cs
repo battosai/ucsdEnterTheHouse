5f69ec4dd7519804bad4c68deb55f6302698266d
Brian Tsai <bt92130@gmail.com>
2016-06-22 22:43:27 -0700

    added end camera and disabled user inputs
    when player unlocks door, this is end game. the camera will switch to
    endCam and user key inputs will be disabled. STILL NEED TO CHECK IF
    "Application.Quit" can be placed where it is right now. ONLY WORKS ONCE
    PROJECT HAS BEEN BUILT.

--- Unity Scripts/PlayerController_WASD.cs
+++ Unity Scripts/PlayerController_WASD.cs
@@ -8,25 +8,30 @@ public class PlayerController_WASD : MonoBehaviour
 	
 	void FixedUpdate() //called just before any physics operations; physics code
 	{
+		bool inputEnabled = GameObject.FindWithTag("Door").GetComponent<doorUnlock>().userInputEnabled;
+		
 		//record when keys are pressed
         bool moveFwd = Input.GetKey(KeyCode.W);
         bool moveBwd = Input.GetKey(KeyCode.S);
 		bool moveLeft = Input.GetKey(KeyCode.A);
 		bool moveRight = Input.GetKey(KeyCode.D);
 		
-		//only move when W, A, S, or D is pressed
-		if(moveFwd)
-			transform.position = transform.position + transform.forward * moveSpeed * Time.deltaTime;
-		else if(moveBwd)
-			transform.position = transform.position + transform.forward * moveSpeed * Time.deltaTime * -1;
-		if(moveLeft)
-			transform.position = transform.position - (transform.right * moveSpeed * Time.deltaTime);
-		else if(moveRight)
-			transform.position = transform.position + (transform.right * moveSpeed * Time.deltaTime);
-		
-		//rotate player along with camera via mouse to allow strafing
-		float horizontal = mouseSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
-		transform.Rotate(0, horizontal, 0, Space.World);
-	
+		//if endgame is reached, user input will be disabled from doorUnlock script
+		if(inputEnabled)
+		{
+			//only move when W, A, S, or D is pressed
+			if(moveFwd)
+				transform.position = transform.position + transform.forward * moveSpeed * Time.deltaTime;
+			else if(moveBwd)
+				transform.position = transform.position + transform.forward * moveSpeed * Time.deltaTime * -1;
+			if(moveLeft)
+				transform.position = transform.position - (transform.right * moveSpeed * Time.deltaTime);
+			else if(moveRight)
+				transform.position = transform.position + (transform.right * moveSpeed * Time.deltaTime);
+			
+			//rotate player along with camera via mouse to allow strafing
+			float horizontal = mouseSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
+			transform.Rotate(0, horizontal, 0, Space.World);
+		}
 	}
 }
--- Unity Scripts/doorUnlock.cs
+++ Unity Scripts/doorUnlock.cs
@@ -4,14 +4,21 @@ using System.Collections;
 public class doorUnlock : MonoBehaviour 
 {	
 	public AudioClip unlockSound;
+	public bool userInputEnabled;
+	Camera mainCam;
+	Camera endCam;
 	AudioSource audio;
 	bool canUnlock;
 
 	// Use this for initialization
 	void Start()
 	{
+		mainCam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+		endCam = GameObject.FindWithTag("EndCamera").GetComponent<Camera>();
+		endCam.enabled = false;
 		audio = GetComponent<AudioSource>();
 		canUnlock = false;
+		userInputEnabled = true;
 	}
 	
 	void OnTriggerEnter(Collider other)
@@ -32,7 +39,14 @@ public class doorUnlock : MonoBehaviour
 		//if in trigger and input is given
 		if(canUnlock && Input.GetKeyDown(KeyCode.Space))
 		{
+				userInputEnabled = false;
 				audio.PlayOneShot(unlockSound, 1);
+				mainCam.enabled = false;
+				endCam.enabled = true;
+				
+				//MIGHT NEED TO MOVE IF IT SIMULTANEOUSLY DOES THIS ALL...
+				//needs to quit after playing the sound in a black screen
+				Application.Quit();
 		}
 	}
 }
