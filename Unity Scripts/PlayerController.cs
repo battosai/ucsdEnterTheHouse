eb3477ecad37ad3b368b699aa097254af5ec6d56
Brian Tsai <bt92130@gmail.com>
2016-06-30 13:37:20 -0700

    added page numbers for menu

--- Unity Scripts/PlayerController.cs
+++ Unity Scripts/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
 	public RawImage currPage;
 	
 	GameObject controls;
+	GameObject pageNum;
 	
 	bool moveFwd;
 	bool moveBwd;
@@ -29,6 +30,7 @@ public class PlayerController : MonoBehaviour
 		
 		controls = GameObject.FindWithTag("Controls");
 		controls.GetComponent<Text>().text = openedMenuText;
+		pageNum = GameObject.FindWithTag("PageNum");
 		currPage = GameObject.FindWithTag("Pages").GetComponent<RawImage>();
 		currPage.texture = controlPage;
 		currPage.enabled = true;
@@ -76,6 +78,24 @@ public class PlayerController : MonoBehaviour
 		bool hasClue1 = GetComponent<iPickYouUp>().hasClue1;
 		bool hasClue2 = GetComponent<iPickYouUp>().hasClue2;
 		bool hasClue3 = GetComponent<iPickYouUp>().hasClue3;
+		int totalPageNum;
+		
+		if(hasClue3)
+		{
+			totalPageNum = 4;
+		}
+		else if(hasClue2)
+		{
+			totalPageNum = 3;
+		}
+		else if(hasClue1)
+		{
+			totalPageNum = 2;
+		}
+		else
+		{
+			totalPageNum = 1;
+		}
 	
 		if(inputEnabled)
 		{
@@ -97,31 +117,42 @@ public class PlayerController : MonoBehaviour
 			{
 				menuOn = !menuOn;
 				currPage.enabled = menuOn;
+				
+				if(menuOn)
+					pageNum.GetComponent<Text>().text = "1/" + totalPageNum;
 			}
 			
 			if(menuOn)
 			{
 				controls.GetComponent<Text>().text = openedMenuText;
 				
-				if(Input.GetKeyDown(KeyCode.Alpha1))
+				if(Input.GetKeyDown(KeyCode.Alpha1) || (!hasClue1 && !hasClue2 && !hasClue3))
 				{
+					pageNum.GetComponent<Text>().text = "1/" + totalPageNum;
 					currPage.texture = controlPage;
 				}
 				else if(Input.GetKeyDown(KeyCode.Alpha2) && hasClue1)
 				{
+					pageNum.GetComponent<Text>().text = "2/" + totalPageNum;
 					currPage.texture = clue1;
 				}
 				else if(Input.GetKeyDown(KeyCode.Alpha3) && hasClue2)
 				{
+					pageNum.GetComponent<Text>().text = "3/" + totalPageNum;
 					currPage.texture = clue2;
 				}
 				else if(Input.GetKeyDown(KeyCode.Alpha4) && hasClue3)
 				{
+					pageNum.GetComponent<Text>().text = "4/" + totalPageNum;
 					currPage.texture = clue3;
 				}
+				
+				pageNum.GetComponent<Text>().text = pageNum.GetComponent<Text>().text.Substring(0, pageNum.GetComponent<Text>().text.Length - 1);
+				pageNum.GetComponent<Text>().text += totalPageNum;
 			}
 			else
 			{
+				pageNum.GetComponent<Text>().text = "";
 				currPage.texture = controlPage;
 				controls.GetComponent<Text>().text = closedMenuText;
 			}
