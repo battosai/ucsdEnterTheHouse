0e7b3c19658ef1377b0b440cf18d959c517858ba
Brian Tsai <bt92130@gmail.com>
2016-07-16 15:32:49 -0700

    added player trigger script/will change for SNAP

---
+++ Unity Scripts/trackPlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Change depending on what you want to measure.
+ * May also be dependent on how SNAP will interface
+ * with Unity.
+*/
+
+public class trackPlayer : MonoBehaviour 
+{
+	GameObject player;
+
+	void Awake()
+	{
+		player = GameObject.FindWithTag ("Player");
+	}
+
+	void Start()
+	{
+		print (Time.time);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject == player) 
+		{
+			print (Time.time);
+		}
+	}
+}
