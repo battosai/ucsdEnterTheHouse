6d3d83a659fb7c2da9a759791735a3daa7b7505d
Brian Tsai <bt92130@gmail.com>
2016-08-01 12:53:58 -0700

    updated trackplayer script, add exe files

--- Unity Scripts/trackPlayer.cs
+++ Unity Scripts/trackPlayer.cs
@@ -1,31 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
+using LSL;
 
-/*
- * Change depending on what you want to measure.
- * May also be dependent on how SNAP will interface
- * with Unity.
-*/
+/*		For narrow scenario: mark when pick up key items, look at different pages,
+ * 		For broad scenario:  have player press key when they have an idea
+ */ 
 
-public class trackPlayer : MonoBehaviour 
-{
-	GameObject player;
+public class trackPlayer : MonoBehaviour {
+	public int scenario;
 
-	void Awake()
-	{
-		player = GameObject.FindWithTag ("Player");
+	liblsl.StreamInfo inf;
+	liblsl.StreamOutlet outl;
+	string[] strings;
+	int i;
+
+	// Use this for initialization
+	void Start () {
+		i = 0;
+		inf = new liblsl.StreamInfo("Test1", "Markers", 1, 0, liblsl.channel_format_t.cf_string, "giu4569");
+		outl = new liblsl.StreamOutlet(inf);
+
+		strings = new string[] { "Test", "ABC", "123" };
 	}
 
-	void Start()
+	// Update is called once per frame
+	void Update () 
 	{
-		print (Time.time);
+		//broad scenario check
+		if (scenario == 0)
+			broadCheck ();
+		
 	}
 
-	void OnTriggerEnter(Collider other)
+	void broadCheck()
 	{
-		if (other.gameObject == player) 
-		{
-			print (Time.time);
-		}
+		if(Input.GetKeyDown(KeyCode.LeftShift))
+			outl.push_sample (strings);
 	}
+
+	//public void lslfnc()
+	//{
+	//	outl.push_sample (strings);
+	//}
 }
